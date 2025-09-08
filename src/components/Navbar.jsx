d90664e86f2cbb94378daecee97ee223e2158831
Taha Siddiqui <tahasiddiqui541@gmail.com>
2025-09-08 22:24:58 +0530

    responsive & navbar completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {assets} from '../assets/assets'
 const Navbar = () => {
+
+  const [showMenu, setshowMenu] = useState(false)
+
+    // Mobile menu would be unable to scroll
+  useEffect(() => {
+    if(showMenu){
+      document.body.style.overflow = 'hidden'
+    }
+    else {
+      document.body.style.overflow = 'auto'
+    }
+    return () => {
+      document.body.style.overflow = 'auto'
+    }
+  },[showMenu])
+
+
   return (
     <div className='absolute top-0 left-0 w-full z-10 '>
         <div className='container mx-auto flex justify-between items-center py-4 px-6 md:px-20 lg:px-32 bg-transparent'>
@@ -15,6 +32,19 @@ const Navbar = () => {
             <button className='hidden md:block bg-white px-8 py-2 rounded-full'>
               Sign up
             </button>
+            <img src={assets.menu_icon} onClick={() => setshowMenu(true)} className='md:hidden w-7 cursor-pointer' alt="" />
+        </div>
+        {/*-------------Mobile menu---------*/}
+        <div className={`md:hidden ${showMenu ? 'fixed w-full' : 'h-0 w-0'} right-0 top-0 bottom-0 overflow-hidden bg-white transition-all`}>
+          <div className='flex justify-end p-6 cursor-pointer'>
+            <img src={assets.cross_icon} onClick={() => setshowMenu(false)} className='w-6' alt="" />
+          </div>
+          <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium'>
+            <a onClick={() => setshowMenu(false)} href="#Header" className='px-4 py-2 rounded-full inline-block'>Home</a>
+            <a onClick={() => setshowMenu(false)} href="#About" className='px-4 py-2 rounded-full inline-block'>About</a>
+            <a onClick={() => setshowMenu(false)} href="#Projects" className='px-4 py-2 rounded-full inline-block'>Projects</a>
+            <a onClick={() => setshowMenu(false)} href="#Testimonials" className='px-4 py-2 rounded-full inline-block'>Testimonials</a>
+          </ul>
         </div>
     </div>
   )
