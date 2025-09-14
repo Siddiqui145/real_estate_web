fc91d21a162002bc9921445a6b1c4ec7f158b7bd
Taha Siddiqui <tahasiddiqui541@gmail.com>
2025-09-14 22:28:59 +0530

    testimonials designed

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import Header from './components/Header'
 import About from './components/About'
 import Projects from './components/Projects'
+import Testimonials from './components/Testimonials'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Header/>
       <About/>
       <Projects/>
+      <Testimonials/>
     </div>
   )
 }
